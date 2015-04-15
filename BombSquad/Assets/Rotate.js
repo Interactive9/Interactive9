bf4b6cf3b624c0e50cf3c85d7083c97654e6c654
ada10fwi@student.lu.e <Navbarq91>
2015-04-15 13:07:23 +0200

    Jag har lagt till så att backåtknappen stänger av unity applikationen. Vi får test i android studio om det funkar.

--- BombSquad/Assets/Rotate.js
+++ BombSquad/Assets/Rotate.js
@@ -1,4 +1,10 @@
 ﻿
 function Update () {
 	transform.Rotate(0, 10*Time.deltaTime, 15*Time.deltaTime);
+	
+	if(Input.GetKeyDown(KeyCode.Escape))
+    {
+        Application.Quit();
+        // or ask to quit
+    }
 }
