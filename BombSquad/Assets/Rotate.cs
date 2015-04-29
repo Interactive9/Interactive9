65c49160543860c312bab8467ab4b2258908d5b8
ada10fwi@student.lu.e <Navbarq91>
2015-04-29 15:31:44 +0200

    Updated cube rotation and added Beep sound effect
    Jag har updaterat rotationen på kuben. Den känns mer naturlig nu, dock
    kan du bara rotera runt y-axeln. har även lagt till en cool ljud effekt
    i bakgrunden.

---
+++ BombSquad/Assets/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Rotate : MonoBehaviour {
+	private Vector3 yAxis, xAxis;
+	// Use this for initialization
+	void Start () {
+		 yAxis = Vector3.up;
+		 //xAxis = Vector3.left;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.touches.Length > 0 && Input.GetTouch(0).phase == TouchPhase.Moved){
+			this.transform.rotation *= Quaternion.AngleAxis(-Input.GetTouch(0).deltaPosition.x, yAxis);
+		}
+	}
+}
